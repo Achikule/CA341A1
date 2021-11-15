fa530e0f5828a6784f6d309f6e6b2e336528cc25
Achikule <achikule279@gmail.com>
2021-11-15 22:58:42 +0000

    Final version

--- bst.py
+++ bst.py
@@ -1,5 +1,6 @@
 class TreeNode:
-    def __init__(self,key,val,left=None,right=None,parent=None):
+    """Creating a node object that accepts dictionaries"""
+    def __init__(self, key, val, left=None, right=None, parent=None):
         self.key = key
         self.payload = val
         self.leftChild = left
@@ -26,59 +27,59 @@ class TreeNode:
 
 
 class BinarySearchTree:
-
+    """BST with insertion and search. Special attributes for setting and getting indexes. Contains for debugging"""
     def __init__(self):
         self.root = None
         self.size = 0
 
-    def put(self,key,val):
+    def insert(self, key, val):
         if self.root:
-            self._put(key,val,self.root)
+            self._insert(key, val, self.root)
         else:
-            self.root = TreeNode(key,val)
+            self.root = TreeNode(key, val)
         self.size = self.size + 1
 
-    def _put(self,key,val,currentNode):
+    def _insert(self, key, val, currentNode):
         if key < currentNode.key:
             if currentNode.hasLeftChild():
-                   self._put(key,val,currentNode.leftChild)
+                self._insert(key, val, currentNode.leftChild)
             else:
-                   currentNode.leftChild = TreeNode(key,val,parent=currentNode)
+                currentNode.leftChild = TreeNode(key, val, parent=currentNode)
         else:
             if currentNode.hasRightChild():
-                   self._put(key,val,currentNode.rightChild)
+                self._insert(key, val, currentNode.rightChild)
             else:
-                   currentNode.rightChild = TreeNode(key,val,parent=currentNode)
+                currentNode.rightChild = TreeNode(key, val, parent=currentNode)
 
-    def __setitem__(self,k,v):
-       self.put(k,v)
+    def __setitem__(self, k, v):
+        self.insert(k, v)
 
-    def get(self,key):
-       if self.root:
-           res = self._get(key,self.root)
-           if res:
-                  return res.payload
-           else:
-                  return None
-       else:
-           return None
+    def search(self, key):
+        if self.root:
+            res = self._search(key, self.root)
+            if res:
+                return res.payload
+            else:
+                return None
+        else:
+            return None
 
-    def _get(self,key,currentNode):
-       if not currentNode:
-           return None
-       elif currentNode.key == key:
-           return currentNode
-       elif key < currentNode.key:
-           return self._get(key,currentNode.leftChild)
-       else:
-           return self._get(key,currentNode.rightChild)
+    def _search(self, key, currentNode):
+        if not currentNode:
+            return None
+        elif currentNode.key == key:
+            return currentNode
+        elif key < currentNode.key:
+            return self._search(key, currentNode.leftChild)
+        else:
+            return self._search(key, currentNode.rightChild)
 
-    def __getitem__(self,key):
-       return self.get(key)
+    def __getitem__(self, key):
+        return self.search(key)
 
-    def __contains__(self,key):
-       if self._get(key,self.root):
-           return True
-       else:
-           return False
+    def __contains__(self, key):
+        if self._search(key, self.root):
+            return True
+        else:
+            return False
 
--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-# This is a sample Python script.
+#!usr/bin/env python3
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from bst import BinarySearchTree
 
-
+# Using built-in dictionaries to store name/number as keys and other two attribute as values
 entries_by_name = {
 
 }
@@ -12,6 +10,16 @@ entries_by_name = {
 entries_by_number = {
 
 }
+
+
+def search(key):
+    try:
+        if key is not None and key in entries_by_name.keys():  # indexing works here since tree has special attribute
+            print(f'Number: {name_tree[key][0]}\nAddress: {name_tree[key][1]}\n')  # __getitem__ defined
+        elif key is not None:
+            print(f'Name: {number_tree[key][0]}\nAddress: {number_tree[key][1]}\n')
+    except TypeError:
+        print("Name/number not found in phonebook. Please check the input and try again\n")
 
 
 entries = int(input("Please enter an integer for the number of entries to add to the phonebook\n"))
@@ -23,33 +31,20 @@ for i in range(entries):
     entries_by_name[f'{name}'] = number, address
     entries_by_number[f'{number}'] = name, address
 
-
 a = [name for name in entries_by_name.items()]
 b = [name for name in entries_by_number.items()]
 name_tree = BinarySearchTree()
 number_tree = BinarySearchTree()
 
-
 for i in range(len(a)):
-    name_tree.put(a[i][0], [a[i][1][0], a[i][1][1]])
-    number_tree.put(b[i][0], [b[i][1][0], b[i][1][1]])
-
-
-def search(key):
-    try:
-        if key is not None and key in entries_by_name.keys():
-            print(f'Number: {name_tree[key][0]}\nAddress: {name_tree[key][1]}\n')
-        elif key is not None:
-            print(f'Name: {number_tree[key][0]}\nAddress: {number_tree[key][1]}\n')
-    except TypeError:
-        print("Name/number not found in phonebook. Please check the input and try again\n")
-
+    name_tree.insert(a[i][0], [a[i][1][0], a[i][1][1]])
+    number_tree.insert(b[i][0], [b[i][1][0], b[i][1][1]])
 
 key = input("Please enter the exact name or number you would like to search for in the phonebook\n"
             "Otherwise, enter 'quit' to exit the program\n")
 
 while key != "quit":
-    key = input("Please enter the exact name or number you would like to search for in the phonebook\n"
-                "Otherwise, enter 'quit' to exit the program\n")
     if key != "quit":
         search(key)
+    key = input("Please enter the exact name or number you would like to search for in the phonebook\n"
+                "Otherwise, enter 'quit' to exit the program\n")
